3db2bdab95075f94b23ddbdf32fd6b82164ac5e8
DennisvanBeelen <dvbeelen@gmail.com>
2020-10-19 14:36:39 +0200

    Initial commit

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,37 @@
+// test examples to look at later
+
+// import { TestBed } from '@angular/core/testing';
+// import { RouterTestingModule } from '@angular/router/testing';
+// import { AppComponent } from './app.component';
+
+// describe('AppComponent', () => {
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       imports: [
+//         RouterTestingModule
+//       ],
+//       declarations: [
+//         AppComponent
+//       ],
+//     }).compileComponents();
+//   });
+
+//   it('should create the app', () => {
+//     const fixture = TestBed.createComponent(AppComponent);
+//     const app = fixture.componentInstance;
+//     expect(app).toBeTruthy();
+//   });
+
+//   it(`should have as title 'IracingApps'`, () => {
+//     const fixture = TestBed.createComponent(AppComponent);
+//     const app = fixture.componentInstance;
+//     expect(app.title).toEqual('IracingApps');
+//   });
+
+//   it('should render title', () => {
+//     const fixture = TestBed.createComponent(AppComponent);
+//     fixture.detectChanges();
+//     const compiled = fixture.nativeElement;
+//     expect(compiled.querySelector('.content span').textContent).toContain('IracingApps app is running!');
+//   });
+// });
